a07d0a19b8e49e3f78fefbe48b1e43212ee43ea5
mamonu <thorosm@msn.com>
2017-01-09 14:34:09 +0000

    added calls plot

---
+++ callsplot.r
@@ -0,0 +1,40 @@
+library(rgl)
+library(reshape)
+rgl.clear(type = c("shapes"))
+calls = read.delim('call_data.tsv', header = T)
+
+bin_size = 0.18
+calls$long_bin =  cut(calls$long, seq(min(calls$long), max(calls$long), bin_size))
+calls$lat_bin =  cut(calls$lat, seq(min(calls$lat), max(calls$lat), bin_size))
+calls$total = log(calls$total) / 2 #need to do this to flatten out totals
+
+calls = melt(calls[,3:5])
+calls = cast(calls, lat_bin~long_bin, fun = sum, fill = 0)
+calls = calls[,2:(ncol(calls)-1)]
+calls = as.matrix(calls)
+
+# simple black and white plot
+x =  (1: nrow(calls))
+z =  (1: ncol(calls))
+rgl.surface(x, z, calls)
+rgl.bringtotop()
+
+
+rgl.pop()
+# nicer colored plot
+ylim <- range(calls)
+ylen <- ylim[2] - ylim[1] + 1
+col <- topo.colors(ylen)[ calls-ylim[1]+1 ]
+
+
+x =  (1: nrow(calls))
+z =  (1: ncol(calls))
+
+rgl.bg(sphere=FALSE, color=c("black"), lit=FALSE)
+rgl.viewpoint( theta = 300, phi = 30, fov = 170, zoom = 0.03)
+rgl.surface(x, z, calls, color = col, shininess = 1)
+rgl.bringtotop()
+
+
+
+
--- norfolkisland.R
+++ norfolkisland.R
@@ -10,13 +10,13 @@ heights = smoothed
 #color=c("black","green")
 #rgl.bg(sphere=TRUE, lit=TRUE, back="filled",fogtype="none",texture=system.file("textures/sunsleep.png",package="rgl"))
 
-rgl.bg(sphere=TRUE, color=c("black","green"), lit=TRUE, back="lines",fogtype="none")
-rgl.bbox(color=c("#333377","white"), emission="#333377", expand=1.03,
-         specular="#3333FF", shininess=5, alpha=0.8 )
+rgl.bg(sphere=TRUE, color=c("black","green"), lit=TRUE, back="lines",fogtype="exp")
+#rgl.bbox(color=c("#333377","white"), emission="#333377", expand=1.03,
+#         specular="#3333FF", shininess=5, alpha=0.8 )
 rgl.clear("lights")
 rgl.light(theta=0,phi=60)
 
-ncolors = 100
+ncolors = 20
 colorlut = terrain.colors(ncolors)
 colorlut[1] = "blue"
 xg = seq(xlim[1],xlim[2],length=n)
@@ -32,11 +32,14 @@ rgl.surface(2*yg, 2*xg, 4*heights, color=col, smooth=TRUE, specular="#000002")
 light3d( diffuse = "yellow", 
         specular = "orange", viewpoint.rel = TRUE) 
 
-for(i in seq(0, 3 * 360, by = 1)) {
-  rgl.viewpoint(theta = i*(10/360), phi = i*12/360,zoom = 0.28,interactive=T,fov = 30)
- 
-}
 
 
 
-#browseURL(paste("file://", writeWebGL(dir=file.path(tempdir(), "webGL"), width=700), sep=""))
+# for(i in seq(0, 3 * 360, by = 1)) {
+#   rgl.viewpoint(theta = i*(10/360), phi = i*12/360,zoom = 0.28,interactive=T,fov = 30)
+#  
+# }
+# 
+
+
+#browseURL(paste("file://", writeWebGL(dir=file.path(tempdir(), "webGL"), width=800), sep=""))
